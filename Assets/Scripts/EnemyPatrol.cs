184e584cd1cd0be28d0919a59e42a36458f81591
Guillaume <guillaume.boeur@gmail.com>
2020-10-27 23:23:30 +0100

    FirstEnemy

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] wayPoints;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destinationPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = wayPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destinationPoint = (destinationPoint + 1) % wayPoints.Length;
+            target = wayPoints[destinationPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
