3095662e954bb78154cffce5d411c581445430c6
Guillaume <guillaume.boeur@gmail.com>
2020-11-07 12:10:46 +0100

    Coins System

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+    public Text coinsCountText;
+
+    public static Inventory instance; // Singleton
+
+    private void Awake() // Singleton
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("There is more than one instance of Inventory in the scene");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public void AddCoins(int count)
+    {
+        coinsCount += count;
+        coinsCountText.text = coinsCount.ToString();
+    }
+}
